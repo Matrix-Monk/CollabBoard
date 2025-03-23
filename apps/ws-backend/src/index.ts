8aaad54e83a2085fe9a4db0a50c2f4e0e824ec82
Matrix-Monk <matrixmonk22@gmail.com>
2025-03-24 01:19:01 +0530

    Init ws and http backend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,16 +1,36 @@
 import ws, { WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+const JWT_SECRET = "u3nviu4n";
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws, request) => {
+  const url = request.url;
 
-    ws.on('error', console.error); 
+  if (!url) {
+    return;
+  }
 
-    ws.on('message', (message) => {
-        console.log('received: %s', message);
-    });
+  const queryParams = new URLSearchParams(url.split("?")[1]);
 
-    ws.send('something');
+  const token = queryParams.get("token") as string;
+
+    if (!token) {
+        ws.close()
+        return
+    }
+
+  const decodedData = jwt.verify(token, JWT_SECRET) as JwtPayload;
+
+  if (!decodedData) {
+        ws.close();
+        return;  }
+
+  ws.on("message", (message) => {
+    console.log("received: %s", message);
+  });
+ 
+  ws.send("something");
 })
 
